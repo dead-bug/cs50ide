e26a4a05093be8d8b0105a906aad132559eb0eb9
Sam White <s.white432@gmail.com>
2017-10-25 08:40:21 +0000

    updated recover1

---
+++ pset4/recover/recover1.c
@@ -0,0 +1,36 @@
+/**
+ * recover.c
+ *
+ * Computer Science 50
+ * Problem Set 4
+ *
+ * Recovers JPEGs from a forensic image.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc != 1)
+    {
+        printf("error");
+        return 1;
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    //int IMGcount = 0;
+    //char *buffer=malloc(sizeof(4));
+    //char buffer=malloc(sizeof(char));
+
+    printf("test\n");
+    for (int i=0; i<10000000; i+=100000)
+    {
+        printf("%d: ",i);
+    int j = fgetc(file);
+
+    if (j != EOF)
+        printf("first byte = %d\n", j);
+    }
+
+}
